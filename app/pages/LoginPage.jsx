05bb27dd61b4988d4989341ac7d61242ea4e6281
Luis Casas <luisscasas45@gmail.com>
2025-01-30 00:04:16 -0600

    FEAT/ login page

---
+++ app/pages/LoginPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Login from "../components/LoginForm";
+
+const LoginPage = () => {
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <div className="bg-gradient-to-r from-[#B0005E] to-[#6C0036] py-4 justify-center items-start p-10 flex-[0.9] hidden md:flex"></div>
+
+      <div className="flex justify-center items-start bg-white p-10 flex-1 ">
+        <div className="w-full max-w-md mt-10">
+          <Login />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
