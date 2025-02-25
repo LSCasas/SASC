5621a68169c24068f21aee7817a1d232cf1d1262
Luis Casas <luisscasas45@gmail.com>
2025-02-24 23:02:02 -0600

    FEAT/ Add getAllStudents function to fetch all students

--- app/api/student.js
+++ app/api/student.js
@@ -24,3 +24,24 @@ export async function createStudent(data) {
     throw error;
   }
 }
+
+// GET ALL STUDENTS
+export async function getAllStudents() {
+  try {
+    const res = await fetch(`${API_URL}/students`, {
+      method: "GET",
+      credentials: "include",
+    });
+
+    const json = await res.json();
+    if (!res.ok) {
+      throw new Error(
+        json.message || "Error desconocido al obtener los estudiantes"
+      );
+    }
+    return json.data;
+  } catch (error) {
+    console.error("Error obteniendo los estudiantes:", error);
+    throw error;
+  }
+}
