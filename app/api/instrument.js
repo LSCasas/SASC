b48794869dd1d245e76b897928d19e983fefe855
Luis Casas <luisscasas45@gmail.com>
2025-02-26 23:38:31 -0600

    FEAT/ Add createInstrument function to handle instrument creation

---
+++ app/api/instrument.js
@@ -0,0 +1,17 @@
+const API_URL = "http://localhost:5000";
+
+// CREATE A NEW INSTRUMENT
+export async function createInstrument(data) {
+  const res = await fetch(`${API_URL}/instrument`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+    credentials: "include",
+  });
+
+  const json = await res.json();
+  if (!json.success) throw new Error(json.error);
+  return json.data;
+}
