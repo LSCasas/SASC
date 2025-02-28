85d54faf926d199170c393f6d71a7a1129c0a16e
Luis Casas <luisscasas45@gmail.com>
2025-02-28 17:44:58 -0600

    CHORE/ replace Sidebar to SidebarAdmin becase of is the perfil of the admin

--- app/pages/formularioDeSedes.jsx
+++ app/pages/formularioDeSedes.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Sidebar from "@/components/Sidebar";
+import SidebarAdmin from "@/components/SidebarAdmin";
 import CampusForm from "@/components/CampusForm";
 
 export default function CampusFormPage() {
   return (
     <div className="flex">
-      <Sidebar />
+      <SidebarAdmin />
       <div className="flex-1 bg-gray-50 min-h-screen">
         <div className="container mx-auto px-6 py-6">
           <h1 className="text-2xl font-bold text-center mb-6 text-[#6C0036]">
