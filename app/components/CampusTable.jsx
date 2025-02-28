1f1bee14431327a347de68558b9bbe2d8764d05e
Luis Casas <luisscasas45@gmail.com>
2025-02-28 17:39:01 -0600

    FEAT/ ActiveCampusTable

--- app/components/CampusTable.jsx
+++ app/components/CampusTable.jsx
@@ -1,41 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getAllCampuses } from "../api/campus";
 import CampusFilters from "./CampusFilters";
 import ExportButtons from "./TeacherExportButtons";
 import Link from "next/link";
 
 export default function CampusTable() {
-  const campus = [
-    {
-      id: 1,
-      campus: "campus Central",
-      address: "Av. Principal 123, Ciudad Central",
-      phone: "123-456-7890",
-    },
-    {
-      id: 2,
-      campus: "campus Norte",
-      address: "Calle Norte 456, Ciudad Norte",
-      phone: "987-654-3210",
-    },
-    {
-      id: 3,
-      campus: "campus Sur",
-      address: "Av. Sur 789, Ciudad Sur",
-      phone: "456-789-0123",
-    },
-    {
-      id: 4,
-      campus: "campus Este",
-      address: "Calle Este 321, Ciudad Este",
-      phone: "321-654-9870",
-    },
-    {
-      id: 5,
-      campus: "campus Oeste",
-      address: "Av. Oeste 789, Ciudad Oeste",
-      phone: "789-012-3456",
-    },
-  ];
+  const [campuses, setCampuses] = useState([]);
+
+  useEffect(() => {
+    const fetchCampuses = async () => {
+      try {
+        const data = await getAllCampuses();
+        const activeCampuses = data.filter((campus) => !campus.isAchive);
+        setCampuses(activeCampuses);
+      } catch (error) {
+        console.error("Error al obtener los campus:", error);
+      }
+    };
+
+    fetchCampuses();
+  }, []);
 
   return (
     <div className="mt-6">
@@ -51,23 +35,21 @@ export default function CampusTable() {
             </tr>
           </thead>
           <tbody>
-            {campus.length > 0 ? (
-              campus.map((campus) => (
+            {campuses.length > 0 ? (
+              campuses.map((campus, index) => (
                 <tr
-                  key={campus.id}
+                  key={campus._id}
                   className="cursor-pointer hover:bg-gray-100"
                 >
+                  <td className="p-3 border-b">{index + 1}</td>{" "}
                   <td className="p-3 border-b">
-                    <Link href="/formularioDeSedes">{campus.id}</Link>
-                  </td>
-                  <td className="p-3 border-b">
-                    <Link href="/formularioDeSedes">{campus.campus}</Link>
+                    <Link href="/formularioDeSedes">{campus.name}</Link>
                   </td>
                   <td className="p-3 border-b">
                     <Link href="/formularioDeSedes">{campus.address}</Link>
                   </td>
                   <td className="p-3 border-b">
-                    <Link href="/formularioDeSedes">{campus.phone}</Link>
+                    <Link href="/formularioDeSedes">{campus.contactPhone}</Link>
                   </td>
                 </tr>
               ))
@@ -85,14 +67,16 @@ export default function CampusTable() {
         <button className="opacity-50 cursor-not-allowed">
           <h1 className="text-black"> ◀︎ </h1>
         </button>
-        <span className="text-gray-600">0–5 de 5</span>
+        <span className="text-gray-600">
+          {campuses.length} de {campuses.length}
+        </span>
         <button className="opacity-50 cursor-not-allowed">
           <h1 className="text-black"> ▶︎ </h1>
         </button>
       </div>
       <div className="mt-3 flex justify-center">
         <div className="w-full">
-          <ExportButtons data={campus} />
+          <ExportButtons data={campuses} />
         </div>
       </div>
     </div>
