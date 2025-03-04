2be4ef05aa73f04531bc1a22ef02f51ca61ef22d
Luis Casas <luisscasas45@gmail.com>
2025-03-04 08:04:03 -0600

    STYLE/ add text-xl movil devices

--- app/pages/formularioDeAlumnos.jsx
+++ app/pages/formularioDeAlumnos.jsx
@@ -8,7 +8,7 @@ export default function StudentFormPage() {
       <Sidebar />
       <div className="flex-1 bg-gray-50 min-h-screen">
         <div className="container mx-auto px-6 py-6">
-          <h1 className="text-2xl font-bold text-center mb-6 text-[#6C0036]">
+          <h1 className="md:text-2xl text-xl font-bold text-center mb-6 text-[#6C0036]">
             Información del Alumno
           </h1>
           <StudentForm />
