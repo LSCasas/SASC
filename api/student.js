f132e0f6e3147f47e80a844d9da8e748de38016b
Luis Casas <luisscasas45@gmail.com>
2025-03-04 20:19:25 -0600

    TEST/ add correct api url

--- api/student.js
+++ api/student.js
@@ -1,9 +1,9 @@
 import { toast } from "react-toastify";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://sasc-api-2.onrender.com";
 
 // DEVELOPMENT:  //http://localhost:5000
-// TESTING: //http://localhost:5000
+// TESTING: //https://sasc-api-2.onrender.com
 
 // CREATE STUDENT
 export async function createStudent(data) {
